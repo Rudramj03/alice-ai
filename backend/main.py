61b9029024357fba6ed1ec3bdff7eafca84ffa11
Rudra Kumar Majhi <rudra.mj.40@gmail.com>
2026-02-14 10:28:54 +0530

    Remove node_modules from repo and apply gitignore

--- backend/main.py
+++ backend/main.py
@@ -29,6 +29,7 @@ app = FastAPI(title="Alice AI Backend (Groq + Web Search)")
 
 app.add_middleware(
     CORSMiddleware,
+    allow_credentials=True,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: process.env.REACT_APP_API_URL
+
 });
 
 export const sendMessage = (message) =>
